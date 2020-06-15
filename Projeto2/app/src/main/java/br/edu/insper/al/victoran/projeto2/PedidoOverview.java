45d140c5a389052b378ebe555b5063f69f3dcccb
Giovanni Santos <leviosa@Giovannis-MacBook-Pro.local>
2020-06-15 16:53:01 -0300

    text file att

--- Projeto2/app/src/main/java/br/edu/insper/al/victoran/projeto2/PedidoOverview.java
+++ Projeto2/app/src/main/java/br/edu/insper/al/victoran/projeto2/PedidoOverview.java
@@ -54,9 +54,9 @@ public class PedidoOverview extends AppCompatActivity {
         end.setOnClickListener(view -> {
             String textFinal = new String();
             String labels = lenghtPadronizer("DESCRITIVO") + lenghtPadronizer("QUANTIDADE") +
-                    lenghtPadronizer("MODELO") + lenghtPadronizer("CÓDIGO") +
-                    lenghtPadronizer("TIPO") + lenghtPadronizer("CATEGORIA") +
-                    lenghtPadronizer("LINHA") + "\n";
+                    lenghtPadronizer("PREÇO") + lenghtPadronizer("MODELO") +
+                    lenghtPadronizer("CÓDIGO") + lenghtPadronizer("TIPO") +
+                    lenghtPadronizer("CATEGORIA") + lenghtPadronizer("LINHA") + "\n";
 
             for (Order order : orders) {
                 Product produto = order.getProduto();
@@ -82,7 +82,8 @@ public class PedidoOverview extends AppCompatActivity {
                             lenghtPadronizer(second);
                     }
                 }
-                String text = descritivo + lenghtPadronizer(quantidade) + 
+                String text = descritivo + lenghtPadronizer(quantidade) +
+                        lenghtPadronizer(("R$" + produto.getprecoCerto().toString())) +
                         lenghtPadronizer(produto.getModelo()) + lenghtPadronizer(produto.getCOD() ) +
                         lenghtPadronizer(produto.getTipo()) + lenghtPadronizer(produto.getCategoria()) +
                         lenghtPadronizer(produto.getLinha()) + "\n";
